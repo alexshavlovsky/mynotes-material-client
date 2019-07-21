a3a349b3ec676564d7380d3115f323cfe96df80b
alexshavlovsky <ctzn@tut.by>
2019-07-21 16:40:15 +0300

    create a separate NgModule for Angular Material components importing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from './material.module';
 
 @NgModule({
   declarations: [
@@ -12,9 +13,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/material.module.ts
@@ -0,0 +1,9 @@
+import {MatButtonModule} from '@angular/material';
+import {NgModule} from '@angular/core';
+
+@NgModule({
+  imports: [MatButtonModule],
+  exports: [MatButtonModule],
+})
+export class MaterialModule {
+}
