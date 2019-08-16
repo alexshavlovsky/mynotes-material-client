1c8b99e792ac63c668b39d37fee640a5eee0c7ad
alexshavlovsky <ctzn@tut.by>
2019-08-16 14:28:08 +0300

    implement slide-out => slide-in router animation

--- src/app/public-forms/common/animations.ts
+++ src/app/public-forms/common/animations.ts
@@ -3,14 +3,14 @@ import {animate, query, stagger, style, transition, trigger} from "@angular/anim
 export const formAnimations = trigger('formAnimations', [
   transition(':enter', [
     query('mat-card', [
-      style({transform: 'translateY(-100vh)'}),
-      stagger(-100, animate('800ms ease-out', style({transform: 'translateY(0)'})))
+      style({transform: 'translateX(100vw)'}),
+      stagger(300, animate('600ms ease-out', style({transform: 'translateX(0)'})))
     ])
   ]),
   transition(':leave', [
     query('mat-card', [
-      style({transform: 'translateY(0px)'}),
-      stagger(100, animate('800ms ease-in', style({transform: 'translateY(-100vh)'})))
+      style({transform: 'translateX(0px)'}),
+      stagger(300, animate('600ms ease-in', style({transform: 'translateX(-100vw)'})))
     ])
   ]),
 ]);
--- src/app/router.animations.ts
+++ src/app/router.animations.ts
@@ -1,17 +1,16 @@
-import {animate, animateChild, group, query as q, sequence, style, transition, trigger} from '@angular/animations';
+import {animate, animateChild, group, query as q, style, transition, trigger} from '@angular/animations';
 
 const query = (s, a, o = {optional: true}) => q(s, a, o);
 
 export const routeAnimations = trigger('routeAnimations', [
   transition('* => *', [
     query(':enter, :leave', style({position: 'fixed', width: '100%'})),
-    query(':enter', style({opacity: 0})),
-    sequence([
+    query(':leave', style({zIndex: 1})),
+    query(':enter', style({zIndex: 2, opacity: 0})),
+    group([
       query(':leave', animateChild()),
-      group([
-        query(':leave', animate(200, style({opacity: 0}))),
-        query(':enter', animate(200, style({opacity: 1}))),
-      ]),
+      query(':leave', animate(1000, style({opacity: 0}))),
+      query(':enter', animate(500, style({opacity: 1}))),
       query(':enter', animateChild()),
     ]),
   ])
