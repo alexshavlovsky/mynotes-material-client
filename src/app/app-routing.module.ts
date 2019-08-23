5e8c57159f4e4993924602c654a4171d43f8ab07
alexshavlovsky <ctzn@tut.by>
2019-08-23 21:53:29 +0300

    apply import().then() lazy loading syntax

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-  {path: '', loadChildren: './auth/auth.module#AuthModule'}
+  {path: '', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)}
 ];
 
 @NgModule({
