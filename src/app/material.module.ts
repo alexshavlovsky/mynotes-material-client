b2fcaf7e9a1d20321eab2297e0135ef7107cdfdb
alexshavlovsky <ctzn@tut.by>
2019-08-27 01:12:36 +0300

    import MatSnackBarModule, MatTooltipModule, MatBadgeModule

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,36 @@
-import {MatButtonModule, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule} from '@angular/material';
+import {
+  MatBadgeModule,
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatSnackBarModule,
+  MatTooltipModule
+} from '@angular/material';
 import {NgModule} from '@angular/core';
 
 @NgModule({
-  imports: [MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule, MatIconModule],
-  exports: [MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule, MatIconModule],
+  imports: [
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatSnackBarModule,
+    MatTooltipModule,
+    MatBadgeModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatSnackBarModule,
+    MatTooltipModule,
+    MatBadgeModule
+  ],
 })
 export class MaterialModule {
 }
