23039a1750c81a1366554c24a0b7759be5d3ce43
alexshavlovsky <ctzn@tut.by>
2019-08-30 14:22:32 +0300

    configure the router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ErrorComponent} from "./error.component";
+import {AuthGuard} from "./guards/auth.guard";
+import {AuthDispatcherGuard} from "./guards/auth-dispatcher.guard";
+import {AuthInverseGuard} from "./guards/auth-inverse.guard";
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: 'auth'},
-  {path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
-  {path: '**', component: ErrorComponent},
+  {
+    path: 'auth',
+    canLoad: [AuthInverseGuard],
+    canActivate: [AuthInverseGuard],
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'notebooks',
+    canLoad: [AuthGuard],
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./notebooks/notebooks.module').then(m => m.NotebooksModule)
+  },
+  {path: '**', component: ErrorComponent, canActivate: [AuthDispatcherGuard]},
 ];
 
 @NgModule({
--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -5,11 +5,11 @@ import {RegisterComponent} from "./components/register/register.component";
 import {AuthComponent} from "./auth.component";
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: 'login'},
   {
     path: '', component: AuthComponent, children: [
       {path: 'login', component: LoginComponent, data: {animation: 'Login'}},
       {path: 'register', component: RegisterComponent, data: {animation: 'Register'}},
+      {path: '', pathMatch: 'full', redirectTo: 'login'},
     ]
   },
 ];
