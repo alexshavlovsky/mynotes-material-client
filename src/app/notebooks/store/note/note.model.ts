75706bd18323900ebd69097339cac0af7c8dafab
alexshavlovsky <ctzn@tut.by>
2019-09-12 23:32:23 +0300

    create adapters for notebook and note api responses

--- src/app/notebooks/store/note/note.model.ts
+++ src/app/notebooks/store/note/note.model.ts
@@ -1,7 +1,19 @@
+import {NoteResponse} from '../../../core/model/note-response.model';
+
 export interface Note {
   id: number;
   title: string;
   text: string;
   notebookId: number;
   lastModifiedOn: Date;
+}
+
+export function noteResponseAdapter(response: NoteResponse): Note {
+  return {
+    id: response.id,
+    title: response.title,
+    text: response.text,
+    notebookId: response.notebookId,
+    lastModifiedOn: response.lastModifiedOn,
+  };
 }
--- src/app/notebooks/store/notebook/notebook.model.ts
+++ src/app/notebooks/store/notebook/notebook.model.ts
@@ -1,5 +1,15 @@
+import {NotebookResponse} from '../../../core/model/notebook-response.model';
+
 export interface Notebook {
   id: number;
   name: string;
   size: number;
+}
+
+export function notebookResponseAdapter(response: NotebookResponse): Notebook {
+  return {
+    id: response.id,
+    name: response.name,
+    size: response.size,
+  };
 }
