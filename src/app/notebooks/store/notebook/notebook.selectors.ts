c37011152adc65ee316fa43e148d1a3e70f8d888
alexshavlovsky <ctzn@tut.by>
2019-09-29 23:18:11 +0300

    implement token based AND boolean logic full text search

--- src/app/notebooks/store/notebook/notebook.selectors.ts
+++ src/app/notebooks/store/notebook/notebook.selectors.ts
@@ -80,29 +80,37 @@ export const storeConsistency = createSelector(
   notebooksConsistency,
   getAllNotebookIds,
   (consistency, notebookIds) => {
-    for (const notebookId of  notebookIds) if (consistency[notebookId] !== true) return false;
+    for (const notebookId of notebookIds) if (consistency[notebookId] !== true) return false;
     return true;
+  }
+);
+
+export const prepareSearchTokens = createSelector(
+  getNotebookSearchQuery,
+  searchQuery => {
+    const trimSpaces = searchQuery.replace(/ {2,}/g, ' ').trim();
+    return trimSpaces === '' ? null : [...new Set(trimSpaces.toLocaleLowerCase().split(' '))];
   }
 );
 
 export const getAllNotebooksFilter = createSelector(
   getAllNotebooks,
   getNotebookSearchMode,
-  getNotebookSearchQuery,
-  (notebooks, mode, query) => {
-    if (mode === false || query === '') return notebooks;
-    const q = query.toLowerCase();
-    return notebooks.filter(nb => nb.name.toLowerCase().includes(q)).slice(0, 100);
+  prepareSearchTokens,
+  (notebooks, mode, tokens) => {
+    if (mode === false || tokens === null) return notebooks;
+    for (const q of tokens) notebooks = notebooks.filter(nb => nb.name.toLowerCase().includes(q));
+    return notebooks.slice(0, 100);
   }
 );
 
 export const getAllNotesFilter = createSelector(
   getAllNotes,
   getNotebookSearchMode,
-  getNotebookSearchQuery,
-  (notes, mode, query) => {
-    if (mode === false || query === '') return [];
-    const q = query.toLowerCase();
-    return notes.filter(n => n.title.toLowerCase().includes(q) || n.text.toLowerCase().includes(q)).slice(0, 100);
+  prepareSearchTokens,
+  (notes, mode, tokens) => {
+    if (mode === false || tokens === null) return [];
+    for (const q of tokens) notes = notes.filter(n => n.title.toLowerCase().includes(q) || n.text.toLowerCase().includes(q));
+    return notes.slice(0, 100);
   }
 );
