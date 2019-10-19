77dede4aba7572f9854bbc9c08e6ded810997193
alexshavlovsky <ctzn@tut.by>
2019-10-20 02:58:33 +0300

    highlight a note in a notebook after navigation from a note card by pressing 'show in notebook' button (note's id is passed in a url fragment)

--- src/app/notebooks/notes-list/notes-nav-list/notes-nav-list.component.ts
+++ src/app/notebooks/notes-list/notes-nav-list/notes-nav-list.component.ts
@@ -2,6 +2,8 @@ import {Component, Input, OnInit} from '@angular/core';
 import {Note} from '../../store/note/note.model';
 import {Observable} from 'rxjs';
 import {AppPropertiesService} from '../../../core/services/app-properties.service';
+import {Location} from '@angular/common';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-notes-nav-list',
@@ -11,11 +13,22 @@ import {AppPropertiesService} from '../../../core/services/app-properties.servic
 export class NotesNavListComponent implements OnInit {
 
   @Input() notes$: Observable<Note[]>;
+  fragment$: Observable<string> = this.route.fragment;
 
-  constructor(private appProps: AppPropertiesService) {
+  constructor(private appProps: AppPropertiesService,
+              private route: ActivatedRoute,
+              private router: Router,
+              private location: Location) {
   }
 
   ngOnInit() {
+  }
+
+  unHighlight() {
+    const pathWithoutFragment = this.location.path(false);
+    console.log(pathWithoutFragment);
+//    this.location.replaceState(pathWithoutFragment);
+    this.router.navigate([pathWithoutFragment]);
   }
 
 }
