b8ed3aa979430929840de557cf36a25c52fc1c2e
alexshavlovsky <ctzn@tut.by>
2019-11-10 21:44:25 +0300

    add dist and dist-bootstrap folders, which contain precompiled material and bootstrap versions of frontend app, to the repository

---
+++ dist-bootstrap/env.js
@@ -0,0 +1,5 @@
+(function (window) {
+  window._env = {
+    apiBaseUrl: 'https://192.168.0.103:8443/api/',
+  };
+}(this));
---
+++ dist/env.js
@@ -0,0 +1,6 @@
+(function (window) {
+  window._env = {
+    apiBaseUrl: 'https://localhost:8443/api/',
+    override: {userPasswordMinLength: 3}
+  };
+}(this));
